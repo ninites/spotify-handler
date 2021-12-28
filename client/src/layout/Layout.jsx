0f24c7e53d54cbbe387f573a4bf67ff4526ab666
Romain <rmnheo@gmail.com>
2021-12-28 13:36:59 +0100

    filter search top bar item template ok

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -7,7 +7,7 @@ import { useContext, useState } from "react";
 import ArtistContext from "../contexts/artists-context";
 
 const Layout = () => {
-  const [artists, setArtists] = useContext(ArtistContext);
+  const [artists] = useContext(ArtistContext);
   const [searchSuggestions, setSearchSuggestions] = useState([]);
   const [searchArtist, setSearchArtist] = useState("");
   const navigate = useNavigate();
@@ -15,12 +15,32 @@ const Layout = () => {
     navigate(url);
   };
 
-  const goToSelected = (name) => {
-    const artistToGo = artists.find((artist) => {
-        return artist.name === name
+  const findArtist = (name) => {
+    return artists.find((artist) => {
+      return artist.name === name;
     });
+  };
+
+  const goToSelectedArtist = (name) => {
+    const artistToGo = findArtist(name);
     const url = "artists/" + artistToGo.id;
     navigate(url);
+  };
+
+  const autoCompleteItemTemplate = (item) => {
+    const artist = findArtist(item);
+    return (
+      <div className="flex">
+        <img
+          src={artist.images[0].url}
+          alt="artist"
+          className="w-1 flex-initial flex align-items-center justify-content-center mr-2"
+        />
+        <p className="flex-initial flex align-items-center justify-content-center">
+          {artist.name}
+        </p>
+      </div>
+    );
   };
 
   const createSuggestions = (event) => {
@@ -51,6 +71,7 @@ const Layout = () => {
       command: () => redirect("artists"),
     },
   ];
+
   return (
     <div>
       <Menubar
@@ -63,9 +84,10 @@ const Layout = () => {
             suggestions={searchSuggestions}
             completeMethod={createSuggestions}
             field="Artiste"
+            itemTemplate={autoCompleteItemTemplate}
             onChange={(e) => setSearchArtist(e.value)}
             onSelect={(e) => {
-              goToSelected(e.value);
+              goToSelectedArtist(e.value);
             }}
           />
         }
